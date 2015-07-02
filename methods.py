02045165699a8246becb83ac99d69089fd645795
fatihsucu <fatihsucu0@gmail.com>
2015-07-02 11:49:57 +0300

    first commit

---
+++ methods.py
@@ -0,0 +1,13 @@
+
+def add(x, y):
+    return x+y
+
+
+def minus(x,y):
+    if x > y:
+        return x - y
+    else:
+        return y - x
+
+def multiply(x,y):
+    return x*y
---
+++ test_methods.py
@@ -0,0 +1,15 @@
+import unittest
+import methods
+
+class TestMethods(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(methods.add(3,5), 8)
+
+    def test_minus(self):
+        self.assertEqual(methods.minus(3,5), 2)
+
+    def test_multiply(self):
+        self.assertEqual(methods.multiply(3,5), 15)
+
+
+unittest.main()
